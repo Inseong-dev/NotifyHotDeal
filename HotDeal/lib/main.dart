5bc1e1f6f0e2829ea4a4624434a5d0f69836ba05
Inseong-dev <73svnptre@gmail.com>
2025-03-09 14:43:44 +0000

    FEAT: 리스트 구현

--- HotDeal/lib/main.dart
+++ HotDeal/lib/main.dart
@@ -1,33 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ItemList())
+      ],
+      child: MyApp(),
+    )
+  );
 }
 
 class ItemList with ChangeNotifier{
-  List<HotDealItem> items = [];
+  List<HotDealItem> _items = [
+    HotDealItem(items: "Hello1",),
+    HotDealItem(items: "Hello2",),
+    HotDealItem(items: "Hello3",),
+    HotDealItem(items: "Hello4",),
+    HotDealItem(items: "Hello5",),
+  ];
 
-  List<HotDealItem> get item => items;
-}
-
-class HotDealItem extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox();
+  int getLength(){
+    return _items.length;
+  }
+  void addItem(){
+    _items.add(HotDealItem(items: "Hello"));
+    notifyListeners();
   }
 }
 
-class MyApp extends StatelessWidget{
+
+class HotDealItem extends StatefulWidget{
+  final items;
+  HotDealItem({required this.items});
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return SizedBox(child:Text(items));
+  // }
+  @override
+  State<HotDealItem> createState() => _HotDealItem();
+}
+
+class _HotDealItem extends State<HotDealItem>{
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(child: Text(widget.items),);
+  }
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<MyApp> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp>{
 
   @override
   Widget build(BuildContext context) {
+    ItemList list = Provider.of<ItemList>(context);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text("HotdealNotizfy",
+              const Text("HotdealNotify",
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.w900,
@@ -36,7 +74,7 @@ class MyApp extends StatelessWidget{
               ),
               Row(
                 children: [
-                  IconButton(onPressed: () => {}, icon: const Icon(Icons.refresh)),
+                  IconButton(onPressed: () => list.addItem(), icon: const Icon(Icons.refresh)),
                   Text("Refresh Time: ",
                   style: TextStyle(
                       fontSize: 10,
@@ -48,9 +86,24 @@ class MyApp extends StatelessWidget{
             ],
           ),
         ),
-        body: ListView.builder(itemBuilder: itemBuilder)
+        body: Container(
+          child: Consumer<ItemList>(
+            builder: (BuildContext context, provider, child){
+              return ListView.separated(
+                itemCount: provider.getLength(),
+                itemBuilder: (BuildContext context, int index){
+                  return provider._items[index];
+                },
+                separatorBuilder: (BuildContext context, int index){
+                  return Divider();
+                }
+              );
+            }
+          )
         ),
       ),
     );
   }
 }
+
+  
